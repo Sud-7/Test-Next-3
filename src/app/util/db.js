1a0ae7678a74c57d370fe1939fe48d5ab2ddead0
Sud <155895839+Sud-7@users.noreply.github.com>
2024-08-20 17:01:24 +0530

    Integrated API with PUT method

--- src/app/util/db.js
+++ src/app/util/db.js
@@ -2,16 +2,19 @@ export const usersData = [
   {
     name: "sid",
     age: 22,
+    city: "North america",
     id: 1,
   },
   {
     name: "peter",
     age: 23,
+    ity: "North america",
     id: 2,
   },
   {
     name: "sam",
     age: 25,
+    ity: "North america",
     id: 3,
   },
 ];
