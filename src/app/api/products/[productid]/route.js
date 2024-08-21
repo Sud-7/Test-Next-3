358cd310b610c1475d4f8a2c3763071dc09e1435
Sud <155895839+Sud-7@users.noreply.github.com>
2024-08-21 14:54:38 +0530

    added and integrated Delete Button

--- src/app/api/products/[productid]/route.js
+++ src/app/api/products/[productid]/route.js
@@ -13,9 +13,18 @@ export async function PUT(request, content) {
 export async function GET(request, content) {
   const productId = content.params.productid;
   const filter = { _id: productId };
-  console.log(filter);
+  //   console.log(filter);
 
   await mongoose.connect(process.env.MONGO_URL);
   const result = await Product.findById(filter);
+  return NextResponse.json({ result });
+}
+export async function DELETE(request, content) {
+  const productId = content.params.productid;
+  const filter = { _id: productId };
+  // console.log(filter);
+
+  await mongoose.connect(process.env.MONGO_URL);
+  const result = await Product.deleteOne(filter);
   return NextResponse.json({ result });
 }
